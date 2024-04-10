5cb6bfea3ecbf88325d014cde8cfcaff93426f36
Buhle Mcetywa <5100149@mypillar7uwc.ac.za>
2024-04-11 00:23:07 +0200

    Added Ui elements

--- WeatherApplication/MainPage.xaml.cs
+++ WeatherApplication/MainPage.xaml.cs
@@ -14,106 +14,124 @@ namespace WeatherApplication
 		private HttpClient _httpClient;
 
 		private double _latestTemperature;
-
-		private int _latestHumidity;
-
-		private float _latestMax;
-
-		private float _latestMin;
-
-		private string _latestDescription;
-
-		private float _latestWindSpeed;
-
-		private float _latestFeelsLike;
-
-		private DateTime _sunrise;
-
-		private DateTime _sunset;
-
-		private int _dateTime;
-
-		private int _latestPressure;
-
-		private GPSModule _gpsmodule;
-
 		public double LatestTemperature
 		{
 			get { return _latestTemperature; }
 			set { _latestTemperature = value; OnPropertyChanged(); }
 		}
 
+		private int _latestHumidity;
+
 		public int LatestHumidity { get { return _latestHumidity; } set { _latestHumidity = value; OnPropertyChanged(); } }
+
+		private float _latestMax;
 
 		public float LatestMax { get { return _latestMax; } set { _latestMax = value; OnPropertyChanged(); } }
 
+		private float _latestMin;
+
 		public float LatestMin { get { return _latestMin; } set { _latestMin = value; OnPropertyChanged(); } }
+
+
+		private string _latestDescription;
 
 		public string LatestDescription { get { return _latestDescription; } set { _latestDescription = value; OnPropertyChanged(); } }
 
+
+		private float _latestWindSpeed;
+
 		public float LatestWindSpeed { get { return _latestWindSpeed; } set { _latestWindSpeed = value; OnPropertyChanged(); } }
+
+		private float _latestFeelsLike;
 
 		public float LatestFeelsLike { get { return _latestFeelsLike; } set { _latestFeelsLike = value; OnPropertyChanged(); } }
 
-		public DateTime Sunrise { get { return _sunrise; } set { _sunrise = value; OnPropertyChanged(); } }
+		private int _latestPressure;
 
-		public DateTime Sunset { get { return _sunset; } set { _sunset = value; OnPropertyChanged(); } }
+		public int LatestPressure { get { return _latestPressure; } set { _latestPressure = value; OnPropertyChanged(); } }
 
-		public int DateTime { get { return _dateTime; } set { _dateTime = value; OnPropertyChanged(); } }
+		private int _clouds;
 
-		public int LatestPressure { get { return _latestPressure; }set { _latestPressure= value; OnPropertyChanged(); } }	
+		public int CloudCoverage { get { return _clouds; } set { _clouds = value; OnPropertyChanged(); } }
 
+		private string _country;
+
+		public string Country { get { return _country; } set { _country = value; OnPropertyChanged(); } }
+
+
+		private string _sunrise;
+
+		public string Sunrise { get { return _sunrise; } set { _sunrise = value; OnPropertyChanged(); } }
+
+		private string _sunset;
+
+		public string Sunset { get { return _sunset; } set { _sunset = value; OnPropertyChanged(); } }
+
+		private string _dateUpdate;
+
+		public string DateUpdate { get { return _dateUpdate; } set { _dateUpdate = value; OnPropertyChanged(); } }
+
+		private GPSModule _gpsmodule;
 
 		public MainPage()
 		{
-			InitializeComponent();
-			RefreshCommand = new Command(GetWeatherForecast);
+			 InitializeComponent();
+			 GetWeatherForecast(_httpClient);
 			_httpClient = new HttpClient();
 			_httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-			
 			GPSModule module = new GPSModule();
 			BindingContext = this;
 
 		}
 
-		public ICommand RefreshCommand { get; set; }
+		
 
 		public async void GetWeatherForecast(object parameters)
 		{
-			
 
-			Location location = await _gpsmodule.GetCurrentLocation();
-			double lat = location.Latitude;
-			double lon = location.Longitude;
+			await DisplayAlert("Notice", "Getting latest forecast", "Okay");
 
-			string appId = "1f7b0a77bf23fecae05e9c7c63d98867";
-			string response = await _httpClient.GetStringAsync(new Uri($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}.99&appid={appId}&units=metric"));
+				Location location = await _gpsmodule.GetCurrentLocation();
+				double lat = location.Latitude;
+				double lon = location.Longitude;
 
-			WeatherInfo responseWeather = JsonConvert.DeserializeObject<WeatherInfo>(response);
+				string appId = "1f7b0a77bf23fecae05e9c7c63d98867";
+				string response = await _httpClient.GetStringAsync(new Uri($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={appId}&units=metric"));
 
-			if (responseWeather != null)
-			{
-				 LatestTemperature=(int)Math.Round(responseWeather.main.temp);
-				 LatestWindSpeed=responseWeather.wind.speed;
-				 LatestMax= responseWeather.main.temp_max;
-				 LatestMin = responseWeather.main.temp_min;
-				 LatestHumidity = responseWeather.main.humidity;
-				 LatestFeelsLike= responseWeather.main.feels_like;
-				 LatestPressure= responseWeather.main.pressure;
-				 
-				if(responseWeather.weather.Count() >= 0)
+				WeatherInfo responseWeather = JsonConvert.DeserializeObject<WeatherInfo>(response);
+
+				if (responseWeather != null)
 				{
-					LatestDescription= responseWeather.weather[0].description.ToUpper();
+					LatestTemperature = (int)Math.Round(responseWeather.main.temp);
+					LatestWindSpeed = responseWeather.wind.speed;
+					LatestMax = responseWeather.main.temp_max;
+					LatestMin = responseWeather.main.temp_min;
+					LatestHumidity = responseWeather.main.humidity;
+					LatestFeelsLike = responseWeather.main.feels_like;
+					LatestPressure = responseWeather.main.pressure;
+				    Country=responseWeather.sys.country;
+				    CloudCoverage = responseWeather.clouds.all;
+				    DateTimeOffset dtOffset = DateTimeOffset.FromUnixTimeSeconds(responseWeather.sys.sunrise);
+				    Sunrise = dtOffset.UtcDateTime.ToString();
+				          _ = DateTimeOffset.FromUnixTimeSeconds(responseWeather.sys.sunset);
+				    Sunset = dtOffset.UtcDateTime.ToString();
+
+				if (responseWeather.weather.Count() >= 0)
+					{
+						LatestDescription = responseWeather.weather[0].description.ToUpper();
+					}
+
+					
+
+
 				}
 
-				DateTimeOffset dtOffset = DateTimeOffset.FromUnixTimeSeconds(responseWeather.sys.sunrise);
-				Sunrise = dtOffset.UtcDateTime;
-				_ = DateTimeOffset.FromUnixTimeSeconds(responseWeather.sys.sunset);
-				Sunset = dtOffset.UtcDateTime; 
-
 				
-			}
+			
+			
 		}
+
+
 	}
 }
 		
